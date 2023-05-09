6113c179d9a16b4be2c1a7b363ab9b12429c68cc
angelrm123 <ar595409@gmail.com>
2023-05-09 11:39:04 -0600

    Estructura de Proyecto Java

--- EmployeesControlller/src/main/java/com/MAXX/EmployeesControlller/Main.java
+++ EmployeesControlller/src/main/java/com/MAXX/EmployeesControlller/Main.java
@@ -1,4 +1,4 @@
-package com.shu.shu;
+package com.MAXX.EmployeesControlller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,25 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Main {
     public static void main(String[] args){
         SpringApplication.run(Main.class,args);
     }
-
-    @GetMapping("/")
-    public String saludos(){
-        return "Hola clase";
-
-    }
-    @GetMapping("/Despedida")
-    public String Despedida(){
-        return "Adios ";
-
-    }
-
-
-
-
 
 }
