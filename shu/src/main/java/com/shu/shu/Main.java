b0f18b455365cdf32e77c09b1d56e971e307ac1f
Xitlalligarciaa <xitlalligarcia2705@gmail.com>
2023-05-09 11:37:33 -0600

    Actualización

--- shu/src/main/java/com/shu/shu/Main.java
+++ shu/src/main/java/com/shu/shu/Main.java
@@ -2,6 +2,7 @@ package com.shu.shu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -9,5 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Main {
     public static void main(String[] args){
         SpringApplication.run(Main.class,args);
+    }
+
+    @GetMapping("/")
+    public String Saludos(){
+        return "Hola Clase";
+    }
+    @GetMapping("/Despedida")
+    public String Despedida(){
+        return "Adios!!";
     }
 }
